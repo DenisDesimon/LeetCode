08e42744fcdcaaca933d983f8e47a51e37123c8b
Denis Desimon <40427257+DenisDesimon@users.noreply.github.com>
2019-07-08 21:56:23 +0300

    #199 Binary Tree Right Side View

---
+++ 199_Binary_Tree_Right_Side_View.cpp
@@ -0,0 +1,57 @@
+//#199 Binary Tree Right Side Viewr - https://leetcode.com/problems/binary-tree-right-side-view/
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <queue>
+using namespace std;
+
+
+ // Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        queue<TreeNode*> nodes;
+        vector<int> result;
+        nodes.push(root);
+        while(!nodes.empty())
+        {
+            int size_nodes = (int)nodes.size();
+            bool level_found = false;
+            while(size_nodes--)
+            {
+                auto cur_node = nodes.front();
+                nodes.pop();
+                if(cur_node == NULL)
+                    continue;
+                if(!level_found)
+                result.push_back(cur_node->val);
+                level_found = true;
+                nodes.push(cur_node->right);
+                nodes.push(cur_node->left);
+            }
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    Solution solution;
+    TreeNode *given_tree = new TreeNode(1);
+    given_tree->left = new TreeNode(2);
+    given_tree->right = new TreeNode(3);
+    given_tree->left->right = new TreeNode(5);
+    given_tree->right->right = new TreeNode(4);
+    vector<int> expected_answer = {1, 3, 4};
+    assert(solution.rightSideView(given_tree) == expected_answer);
+
+    return 0;
+}
+
