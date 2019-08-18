e29d33235e3e64ba6ce199f95fa966ab41d17d3d
Denis Desimon <40427257+DenisDesimon@users.noreply.github.com>
2019-08-18 11:36:31 +0300

    #153 Find Minimum in Rotated Sorted Array

---
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,49 @@
+//#153 Find Minimum in Rotated Sorted Array - https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+#include <iostream>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        int result = nums[left];
+        while(left <= right)
+        {
+            int mid = (left + right) / 2;
+            if(nums[left] <= nums[mid])
+            {
+                result = min(result, nums[left]);
+                if(nums[mid] > nums[right])
+                    left = mid + 1;
+                else
+                    break;
+            }
+            else
+            {
+                result = min(result, nums[mid]);
+                if(nums[left] > nums[mid])
+                    right = mid - 1;
+                else
+                    break;
+            }
+        }
+        return result;
+    }
+};
+
+
+int main()
+{
+    Solution solution;
+    vector<int> given_nums = {2, 1};
+    int expected_answer = 1;
+    assert(solution.findMin(given_nums) == expected_answer);
+
+    given_nums = {4, 5, 6, 7, 0, 1, 2};
+    expected_answer = 0;
+    assert(solution.findMin(given_nums) == expected_answer);
+
+    return 0;
+}
